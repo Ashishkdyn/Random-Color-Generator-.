74bc954e87dac19cfa86cd20c4df2cb06b43272f
Ashishkdyn <132130441+Ashishkdyn@users.noreply.github.com>
2023-05-10 04:46:44 +0530

    Create BLOG.js

---
+++ BLOG.js
@@ -0,0 +1,13 @@
+const getColor = () => {
+  const random = Math.floor(Math.random() * 16777215);
+  const number = document.getElementById("color-id");
+  const changeColor = (number.textContent = `#${random.toString(16)}`);
+  document.body.style.background = changeColor;
+  navigator.clipboard.writeText(changeColor);
+};
+
+//event call
+document.getElementById("btn").addEventListener("click", getColor);
+
+//initial call
+getColor();
